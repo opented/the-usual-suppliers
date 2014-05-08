276c0fcb7da82f95531f01e7b987dca4bf75b3d0
Friedrich Lindenberg <friedrich@pudo.org>
2014-05-08 11:59:43 +0200

    initial checkin

---
+++ README.md
@@ -0,0 +1,8 @@
+# The Usual Suppliers
+
+Who are the biggest recipients of public contracts within the European Union?
+
+Project from the #dhplus hackday 2014.
+
+
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tus',
+    version='0.1',
+    description="The Usual Suppliers",
+    long_description='',
+    classifiers=[
+        ],
+    keywords='',
+    author='',
+    author_email='friedrich@pudo.org',
+    url='http://pudo.org',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+    ],
+    tests_require=[],
+    entry_points=''
+)
